80288d703106da1209a370b7bfe3ccf400246f18
Chitchula-Sai-Bhuvan <cb.sc.p2cse25007@cb.students.amrita.edu>
2025-09-05 00:37:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  // Create an array of numbers from 1 to 10
+  const numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Natural Numbers from 1 to 10</h1>
+      <ul>
+        {numbers.map((num) => (
+          <li key={num}>{num}</li>
+        ))}
+      </ul>
     </div>
   );
 }
